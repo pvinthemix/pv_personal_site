5e8f3087cb6219acfe6ee985db8d932271bd0cee
Paul Vangelakos <paulvangelakos@gmail.com>
2019-04-10 12:24:32 -0600

    more styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,12 @@ class index extends Component {
           <div className='user-image'>
             <img className='headshot' src={headshot}></img>
           </div>
-          <h1 className='user-name'>@pvinthemix</h1>
-          <h2 className='title'>Front-End Web Developer</h2>
+          <div className='user-name-div'>
+            <h1 className='user-name'>@pvinthemix</h1>
+          </div>
+          <div className='user-title'>
+            <h2 className='title'>Front-End Web Developer</h2>
+          </div>
           <div className='social-links'></div>
         </div>
       </div>
