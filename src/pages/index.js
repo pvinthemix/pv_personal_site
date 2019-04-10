98757bf23cabf1c1fadfde5c94e02f5671cdf29d
Paul Vangelakos <paulvangelakos@gmail.com>
2019-04-10 12:12:50 -0600

    Adding headshot and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import '../styles/index.css';
-
+import headshot from '../styles/images/headshot.png'
 
 class index extends Component {
-  constructor() {
-    super()
-  }
 
   render() {
     return (
       <div className='landing'>
-        <div className='main'>hello</div>
+        <div className='main'>
+          <div className='user-image'>
+            <img className='headshot' src={headshot}></img>
+          </div>
+          <h1 className='user-name'>@pvinthemix</h1>
+          <h2 className='title'>Front-End Web Developer</h2>
+          <div className='social-links'></div>
+        </div>
       </div>
     );
   }
