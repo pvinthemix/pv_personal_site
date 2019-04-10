a4538e69bb4433df46ffb66d335d77ed85e18080
Paul Vangelakos <paulvangelakos@gmail.com>
2019-04-10 11:15:16 -0600

    Initial Commit

---
+++ src/pages/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+
+
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
