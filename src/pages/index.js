07748795f05d5c7672e7676aad4a5f0e66e320d4
Paul Vangelakos <paulvangelakos@gmail.com>
2019-04-10 11:52:18 -0600

    Setting up files

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import '../styles/index.css';
+
+
+class index extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className='landing'>
+        <div className='main'>hello</div>
+      </div>
+    );
+  }
+}
+
+export default index;
